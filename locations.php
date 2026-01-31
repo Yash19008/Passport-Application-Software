b5c234ad4d308262cfb485ce522672692a123805
Yash Soni <yash190068@gmail.com>
2026-01-31 22:32:54 +0530

    New Updates

--- locations.php
+++ locations.php
@@ -26,7 +26,7 @@ $rpos = $conn->query("SELECT * FROM rpo_offices ORDER BY name ASC");
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Locations</li>
+                        <li class="breadcrumb-item active">Manage Locations</li>
                     </ol>
                 </div>
             </div>
